b5ff6445f5defe5650e484732586bc7ef3431ebd
nullelement <nullelement-heroku@yahoo.com.br>
2016-09-24 11:14:00 -0300

    Persiste a inscrição. Ajustes de identação de tab para espaço.

--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -6,37 +6,41 @@ from django.shortcuts import render
 from django.template.loader import render_to_string
 
 from eventex.subscriptions.forms import SubscriptionForm
+from eventex.subscriptions.models import Subscription
+
 
 def subscribe(request):
-	if request.method == 'POST':
-		return create(request)
-	else:
-		return new(request)
+    if request.method == 'POST':
+        return create(request)
+    else:
+        return new(request)
 
 
 def create(request):
-	form = SubscriptionForm(request.POST)
+    form = SubscriptionForm(request.POST)
 
-	if not form.is_valid():
-		return render(request, 'subscriptions/subscription_form.html',
-					  {'form': form})
+    if not form.is_valid():
+        return render(request, 'subscriptions/subscription_form.html',
+                      {'form': form})
 
-	_send_mail('Confirmação de inscrição',
-			   settings.DEFAULT_FROM_EMAIL,
-			   form.cleaned_data['email'],
-			   'subscriptions/subscription_email.txt',
-			   form.cleaned_data)
+    _send_mail('Confirmação de inscrição',
+               settings.DEFAULT_FROM_EMAIL,
+               form.cleaned_data['email'],
+               'subscriptions/subscription_email.txt',
+               form.cleaned_data)
 
-	messages.success(request, 'Inscrição realizada com sucesso!')
+    Subscription.objects.create(**form.cleaned_data)
 
-	return HttpResponseRedirect('/inscricao/')
+    messages.success(request, 'Inscrição realizada com sucesso!')
+
+    return HttpResponseRedirect('/inscricao/')
 
 
 def new(request):
-	return render(request, 'subscriptions/subscription_form.html',
-				  {'form': SubscriptionForm()})
+    return render(request, 'subscriptions/subscription_form.html',
+                  {'form': SubscriptionForm()})
 
 
 def _send_mail(subject, from_, to, template_name, context):
-	body = render_to_string(template_name, context)
-	mail.send_mail(subject, body, from_, [from_, to])
+    body = render_to_string(template_name, context)
+    mail.send_mail(subject, body, from_, [from_, to])
